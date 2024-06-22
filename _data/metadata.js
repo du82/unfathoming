f87ff71dfb4ee85ede936b356618b2e7968392dc
du82 <ducheng0@protonmail.com>
2024-06-22 04:17:42 -0500

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,10 @@ module.exports = {
 	title: "Unfathoming",
 	url: "https://unfathom.ing/",
 	language: "en",
-	description: "I am writing about my experiences.",
+	description: "A blog about the life experiences, thoughts, and ideas of a computer scientist with a strange life.",
 	author: {
 		name: "Du Cheng",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		email: "493@tuta.io",
+		url: "https://unfathom.ing/about-me/"
 	}
 }
