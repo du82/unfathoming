f9939dc78a3d76c0f44cb6de8d747b51098bfc89
du82 <65834069+du82@users.noreply.github.com>
2024-07-19 00:00:28 -0500

    hopefully fix?

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
+		"./assets/": "/assets",
 		"./public/": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css",
 		"./content/robots.txt": "/robots.txt",
